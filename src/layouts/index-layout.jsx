130ce02ad37404f644fd6dffcd2e90bb7833e766
Tiago Zardetto <tiagohourneaux@gmail.com>
2026-02-09 13:17:17 -0300

    refactor: simplify index layout and use new color variables

--- src/layouts/index-layout.jsx
+++ src/layouts/index-layout.jsx
@@ -2,11 +2,9 @@ import { Outlet } from "react-router";
 
 const IndexLayout = () => {
 	return (
-		<>
-			<div className="h-screen w-screen overflow-x-hidden bg-[#051e4c]">
-				<Outlet />
-			</div>
-		</>
+		<div className="bg-nba-deep min-h-screen w-screen overflow-x-hidden">
+			<Outlet />
+		</div>
 	);
 };
 
